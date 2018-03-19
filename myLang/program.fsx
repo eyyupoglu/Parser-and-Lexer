3a2f49ebe730295a115d40abf20195229d5a5c98
mehmet eyyupoğlu <eyyupoglu.mehmet@gmail.com>
2018-03-19 20:36:41 +0100

    Add files via upload

---
+++ myLang/program.fsx
@@ -0,0 +1,31 @@
+#r "FsLexYacc.Runtime.7.0.6/lib/portable-net45+netcore45+wpa81+wp8+MonoAndroid10+MonoTouch10/FsLexYacc.Runtime.dll"
+open Microsoft.FSharp.Text.Lexing
+open System
+
+#load "AST.fs"
+open AST
+
+#load "parser.fs"
+open parser
+#load "lexer.fs"
+open lexer
+
+
+
+let parse input =
+    // translate string into a buffer of characters
+    let lexbuf = LexBuffer<char>.FromString input
+    // translate the buffer into a stream of tokens and parse them
+    let res = parser.start lexer.tokenize lexbuf
+    // return the result of parsing (i.e. value of type "expr")
+    res
+
+let rec compute n =
+    try
+    let e = parse (Console.ReadLine())
+    printfn "ok"
+
+    with err -> printfn "ko"
+
+// Start interacting with the user
+compute 3
