36c273737b306e41a12e7764c02ae215336f3e52
unknown <eyyupoglu.mehmet@gmail.com>
2018-04-09 18:23:47 +0200

    almost working

--- myLang/program.fsx
+++ myLang/program.fsx
@@ -1,17 +1,12 @@
 #r "FsLexYacc.Runtime.7.0.6/lib/portable-net45+netcore45+wpa81+wp8+MonoAndroid10+MonoTouch10/FsLexYacc.Runtime.dll"
 open Microsoft.FSharp.Text.Lexing
 open System
-
 #load "AST.fs"
 open AST
-
 #load "parser.fs"
 open parser
 #load "lexer.fs"
 open lexer
-
-
-
 let parse input =
     // translate string into a buffer of characters
     let lexbuf = LexBuffer<char>.FromString input
@@ -19,15 +14,14 @@ let parse input =
     let res = parser.start lexer.tokenize lexbuf
     // return the result of parsing (i.e. value of type "expr")
     res
-
+//input
 let text  = parse (Console.ReadLine())
-
 printfn "\n\n"
-//helping function for creating an edge
+//helping
 let rec edge start endd act = 
     String.Format("q{0} ->  q{1} [label = \"{2}\"];\n", start, endd, act)
-
-
+//helping function
+let printSeq seq1 = Seq.iter (printf "%A ") seq1; printfn ""
 let rec arithm = function
     | N (int) -> string int 
     | X str -> string str
@@ -38,7 +32,6 @@ let rec arithm = function
     | UPlus a -> "+" + arithm a 
     | UMinus a -> "-" + arithm a
     | _ -> "error"
-
 let rec bool = function
     | True -> string true
     | False -> string false
@@ -54,46 +47,41 @@ let rec bool = function
     | Smaller ( a , b ) -> arithm a + "<" + arithm b
     | SmallerOrEqual ( a , b ) -> arithm a + "<=" + arithm b
     | Nested b -> bool b 
-
-
-let mutable nat =  seq []
-let mutable counter = seq []
-
+let mutable states =   []
 //This function finds the last element of a list
 let rec last tmp = function
     | [] -> tmp
     | a::b -> last a b 
-
-let rec compile (s: int)= function
+let rec findMax (n: int) = function
+    | [] -> n
+    | a::b when a > n -> findMax a b
+    | a::b -> findMax n b
+let newStateNumber liste = (findMax 0 liste) + 1
+let addStateToStateList stateList stateNumber = List.append stateList [stateNumber]
+let rec compile (s: int) (e: int) = function
     | Assign ( str, num )   ->  
-        nat <- Seq.append (nat) (seq [edge (string s) (string (s+1)) ( str + ":=" + (arithm num))])
-        counter <- Seq.append (counter) (seq [s])
-        edge (string s) (string (s+1)) ( str + ":=" + (arithm num)) 
+        let newNode = newStateNumber states//fresh q
+        states <- addStateToStateList states newNode//returns new list of states
+        edge (string s) (string e) ( str + ":=" + (arithm num)) 
     | Skip                  ->
-        nat <- Seq.append (nat) (seq [edge (string s) (string (s+1)) "skip"])
-        counter <- Seq.append (counter) (seq [s])
-        edge (string s) (string (s+1)) "skip"
+        let newNode = newStateNumber states//fresh q
+        states <- addStateToStateList states newNode//returns new list of states
+        edge (string s) (string e) "skip"
     | Command (c1, c2 )     ->  
-        compile s c1 + compile (s+1) c2 
-    | If (gc)               ->  compile2 (s)  gc 
-    | Do (gc)               ->  compile2 (s)  gc 
-and compile2 s  = function
-    | SingletonGuarded (b, c) ->
-        nat <- Seq.append (nat) (seq [edge (string s) (string (s+1)) (bool b)])
-        counter <- Seq.append (counter) (seq [s])
-        let lastOne = string (last (Seq.toList nat))
-        edge (string s) (string (s+1)) (bool b) + 
-        compile (s+1)  c 
-        
-    | Associate (gc1 ,gc2 )   -> compile2 (s+1)  gc1  + compile2 (s+2)  gc2 
-
-//helping function
-let printSeq seq1 = Seq.iter (printf "%A ") seq1; printfn ""
+        let newNode = newStateNumber states//fresh q 
+        compile s newNode c1 + compile newNode e c2
+    | Do (gc)               ->  compileGuarded s e gc
+and compileGuarded s e = function
+    | SingletonGuarded (b, c) ->       
+        let newNode = newStateNumber states//fresh q
+        states <- addStateToStateList states newNode//returns new list of states
+        edge (string s) (string (newNode)) (bool b) + compile newNode s c 
 
 
-printfn "%A" (compile 0  text)
-nat |> Seq.iter (fun x -> printf "%s " x)
-printSeq counter
+printfn "%A" text
+printfn "%A" (compile 0 87 text)
+states |> List.iter (fun x -> printf "%d " x)
+printSeq states
 
 
 
